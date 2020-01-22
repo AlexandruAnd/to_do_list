5e11a82d94b541fa99b4930f4b68e0b33d9ad4b8
Alex <andreica.alexandru2000@gmail.com>
2020-01-22 20:30:21 +0200

    added service layer

---
+++ src/main/java/org/fasttrackit/service/TaskService.java
@@ -0,0 +1,37 @@
+package org.fasttrackit.service;
+
+import org.fasttrackit.domain.Task;
+import org.fasttrackit.persitance.TaskRepository;
+import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TaskService {
+
+    private TaskRepository taskRepository = new TaskRepository();
+    // apply any other business logic in the service layer
+    public void createTask (CreateTaskRequest request) throws IOException, SQLException {
+        System.out.println("Creating task :" + request);
+        taskRepository.createTask(request);
+    }
+
+
+    public void updateTask(long id, UpdateTaskRequest request) throws IOException, SQLException {
+        System.out.println("Updateing task :" + id + " " + request);
+        taskRepository.updateTask(id,request);
+    }
+
+    public void deleteTask(long id) throws IOException, SQLException {
+        System.out.println("Deleting task :" + id);
+        taskRepository.deleteTask(id);
+
+    }
+    public List<Task> getTasks() throws IOException, SQLException {
+        System.out.println("Retrieving all tasks.");
+        return taskRepository.getTasks();
+
+    }
+}
